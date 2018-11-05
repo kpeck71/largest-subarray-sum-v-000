d012388d5f3d204c11101ff1d0a130d06df969a2
Kristen Peck <kpeck71@gmail.com>
2018-11-05 18:02:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ function largestSubarraySum(nums) {
     if (max_so_far < max_ending_here) {
       max_so_far = max_ending_here
     }
+    //We keep a variable to store the current sum. If the sum is below zero, then we reset it to the current number (start from current number).
     if (max_ending_here < 0) {
       max_ending_here = 0
-      //We keep a variable to store the current sum. If the sum is below zero, then we reset it to the current number (start from current number).
+
     }
   }
   return max_so_far
