7ecbd65f7108149c7492a0e90c08287d80da5bde
Kristen Peck <kpeck71@gmail.com>
2018-11-05 18:02:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function largestSubarraySum(nums) {
     }
     if (max_ending_here < 0) {
       max_ending_here = 0
+      //We keep a variable to store the current sum. If the sum is below zero, then we reset it to the current number (start from current number).
     }
   }
   return max_so_far
