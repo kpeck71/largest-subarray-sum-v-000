d2154ad70434297f45da0096a6f77248064d118b
Kristen Peck <kpeck71@gmail.com>
2018-11-05 18:08:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ function largestSubarraySum(nums) {
   }
   return max_so_far
 }
+
+// function largestSubarraySum(arr) {
+//   let globalLargest = 0;
+//   let localLargest = arr[0];
+//   const subArray = arr.slice(1, arr.length - 1);
+//    subArray.forEach(n => {
+//     localLargest += n;
+//     if (localLargest > globalLargest) { globalLargest = localLargest };
+//   });
+//    return globalLargest
+// }
